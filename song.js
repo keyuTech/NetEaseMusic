64108e1fedfa23f654aaa14a83d423e39eff6332
wky <wky_0615@163.com>
2017-12-06 14:25:37 +0800

    play/pause

--- song.js
+++ song.js
@@ -14,8 +14,26 @@ $(function(){
       if(str){
         let $p = $("<p></p>")
         $p.attr("data-time", str.time).text(str.contents)
-        $p.appendTo($lyric)
+        $p.appendTo($lyric.children('.lines'))
       }
     })
+  })
+
+  let audio = new Audio
+  audio.autoplay = true
+  audio.src = "http://dl.stream.qqmusic.qq.com/C400001CFYXK4afx2j.m4a?vkey=56A52BF4F5355C7B487DC2D192F5784C2A8F7A986561FFAFE1DED174F29940197A1EA9E7E0A46203E171BB1994E6F0FCFEAB7F8E1B7CE002&guid=8994004800&uin=0&fromtag=66"
+  audio.oncanplay = function(){
+    $('.disc-container').addClass('playing')
+  }
+  $('.disc').on('click', function(){
+    if(audio.paused) {
+      audio.play()
+      $('.pause').removeClass('pausing')
+      $('.disc-container').addClass('playing')
+    }else{
+      audio.pause()
+      $('.pause').addClass('pausing')
+      $('.disc-container').removeClass('playing')
+    }
   })
 })
