018c70fb2c9ca8c94f56142c37e97f987da20c29
wky <wky_0615@163.com>
2017-12-16 15:12:15 +0800

    update hotsong js

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ $(function(){
     items.forEach((i) => {
       let $li = $(`
       <li>
-      <a href="./song.html?id=${i.id}">
-      <p class="song-title">${i.title}</p>
-        <p class="song-details">
-          <svg class="icon icon-sq hide" aria-hidden="true">
-            <use xlink:href="#icon-sq"></use>
+        <a href="./song.html?id=${i.id}">
+        <p class="song-title">${i.title}</p>
+          <p class="song-details">
+            <svg class="icon icon-sq hide" aria-hidden="true">
+              <use xlink:href="#icon-sq"></use>
+            </svg>
+            ${i.author}-${i.title}
+          </p>
+          <svg class="icon icon-play" aria-hidden="true">
+            <use xlink:href="#icon-play"></use>
           </svg>
-          ${i.author}-${i.title}
-        </p>
-        <svg class="icon icon-play" aria-hidden="true">
-          <use xlink:href="#icon-play"></use>
-        </svg>
-      </a>
-    </li>
-      `)
+        </a>
+      </li>
+        `)
     $(".lastestMusic").append($li)
     })
     $(".loading").addClass('hide')
@@ -34,7 +34,48 @@ $(function(){
   })
 
   $('.tab-nav').on('tabChange', function(e, index){
-    console.log(e, index)
+    let $li = $('#tabs>li').eq(index)
+    if(index === 1){
+      if($li.attr('downloaded') === 'yes'){
+        return
+      }
+      $.get('./hotsongs.json').then((response) => {
+        response.forEach((i) => {
+          let $id
+          if(i.id < 10){
+            $id = '0' + i.id
+          }else{
+            $id = i.id
+          }
+          let $template = $(`
+          <li class="hot-song">
+            <a href="./song.html?id=${i.id}&label=hotsong">
+              <p class="rank-number">${$id}</p>
+              <div class="song-content">
+                <p class="song-title">${i.title}</p>
+                <p class="song-details">
+                  <svg class="icon icon-sq" aria-hidden="true">
+                    <use xlink:href="#icon-sq"></use>
+                  </svg>
+                  ${i.author}-${i.title}
+                </p>
+                <svg class="icon icon-play" aria-hidden="true">
+                  <use xlink:href="#icon-play"></use>
+                </svg>
+              </div>
+            </a>
+          </li>`)
+        $(".hot-songs>ul").append($template)
+        })
+        $('#tabs>li').eq(index).attr('downloaded', 'yes')
+        $(".loading").addClass('hide')
+        $('.hot-song').slice(0, 3).addClass('rank-top')
+      })
+    }else if(index === 2){
+
+    }
   })
+
+
 })
 
--- song.js
+++ song.js
@@ -1,13 +1,25 @@
 $(function(){
-  let id = parseInt(location.search.match(/\bid=([^&]*)/)[1])
-  $.get("./songs.json").then(function(response){
-      let songs = response
-      let song = songs.filter((s) => {return s.id === id})[0]
-      let {url, cover, title, author, lyric} = song
-      initPlayer(url, cover)
-      setDetails(title, author, lyric)
-    }
-  )
+  if(location.search.match(/&label=hotsong/)){
+    let id = parseInt(location.search.match(/\bid=([^&]*)/)[1])
+    $.get("./hotsongs.json").then(function(response){
+        let songs = response
+        let song = songs.filter((s) => {return s.id === id})[0]
+        let {url, cover, title, author, lyric} = song
+        initPlayer(url, cover)
+        setDetails(title, author, lyric)
+      }
+    )
+  }else{
+    let id = parseInt(location.search.match(/\bid=([^&]*)/)[1])
+    $.get("./songs.json").then(function(response){
+        let songs = response
+        let song = songs.filter((s) => {return s.id === id})[0]
+        let {url, cover, title, author, lyric} = song
+        initPlayer(url, cover)
+        setDetails(title, author, lyric)
+      }
+    )
+  }
 
   function initPlayer(url, cover){
     let audio = new Audio
