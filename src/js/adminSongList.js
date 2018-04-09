742a9613369541123687765c68292e87c216d6d2
wky0615 <wky_0615@163.com>
2018-04-09 10:02:31 +0800

    获取歌曲列表，点击添加active

--- src/js/adminSongList.js
+++ src/js/adminSongList.js
@@ -1,17 +1,17 @@
 {
   let view = {
     el: '.songListContainer',
-    init(){
+    init() {
       this.$el = $(this.el)
     },
     template: `
     <ul class="songList">
     </ul>
     `,
-    render(data){
+    render(data) {
       this.init()
       this.$el.html(this.template)
-      let {songs} = data
+      let { songs } = data
       let liList = songs.map((song) => {
         let li = $('<li></li>').text(song.name)
         return li
@@ -21,21 +21,54 @@
         this.$el.find('ul').append(li)
       })
     },
-    removeActive(){
+    activeItem(li) {
+      let $li = $(li)
+      $li.addClass('active').siblings('.active').removeClass('active')
+    },
+    removeActive() {
       this.$el.find('.active').removeClass('active')
     }
   }
   let model = {
     data: {
       songs: []
+    },
+    find() {
+      let query = new AV.Query('Song')
+      return query.find().then((songs) => {
+        this.data.songs = songs.map((song) => {
+          return {
+            id: song.id,
+            name: song.attributes.name,
+            singer: song.attributes.singer,
+            url: song.attributes.url
+          }
+        })
+        return songs
+      })
     }
   }
   let controller = {
-    init(view, model){
+    init(view, model) {
       this.view = view
       this.model = model
       this.view.render(this.model.data)
-      window.eventHub.on('upload', ()=>{
+      this.getAllSongs()
+      this.bindEvents()
+      this.bindEventHub()
+    },
+    getAllSongs() {
+      return this.model.find().then(() => {
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvents() {
+      this.view.$el.on('click', 'li', (e) => {
+        this.view.activeItem(e.currentTarget)
+      })
+    },
+    bindEventHub() {
+      window.eventHub.on('upload', () => {
         this.view.removeActive()
       })
       window.eventHub.on('create', (data) => {
